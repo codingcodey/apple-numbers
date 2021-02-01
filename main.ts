16195cc436cb07f3a06b4cb928cc019cc55e50ac
codingcodey <76607086+codingcodey@users.noreply.github.com>
2021-02-01 22:14:15 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let mySprite: Sprite = null
+let gap = 20
+for (let indexX = 0; indexX <= 4; indexX++) {
+    for (let indexY = 0; indexY <= 5; indexY++) {
+        mySprite = sprites.create(img`
+            . . . . . . . e c 7 . . . . . . 
+            . . . . e e e c 7 7 e e . . . . 
+            . . c e e e e c 7 e 2 2 e e . . 
+            . c e e e e e c 6 e e 2 2 2 e . 
+            . c e e e 2 e c c 2 4 5 4 2 e . 
+            c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+            . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+            . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+            . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+            . . . 2 2 e e 4 4 4 2 e e . . . 
+            . . . . . 2 2 e e e e . . . . . 
+            `, SpriteKind.Food)
+        mySprite.setPosition(40 + indexX * gap, 10 + indexY * gap)
+    }
+}
